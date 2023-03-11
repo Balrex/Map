c73df0016f3605c77f42c816b7a31e8f154d82db
Борис <118976669+Balrex@users.noreply.github.com>
2023-03-11 13:18:37 +0300

    Update node.java

--- src/node.java
+++ src/node.java
@@ -1,14 +1,13 @@
 public class node<T1, T2>{
     T1 key;
     T2 data;
-
-    node prev;
+    int height;
+    
     node left;
     node right;
-    public node (T1 key, T2 data, node<T1, T2> prev, node<T1, T2> left, node<T1, T2> right){
+    public node (T1 key, T2 data, node<T1, T2> left, node<T1, T2> right){
         this.key=key;
         this.data=data;
-        this.prev=prev;
         this.left=left;
         this.right=right;
     }
